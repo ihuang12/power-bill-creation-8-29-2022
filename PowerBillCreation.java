07b05b74690999eb1b43ad2467bc4e6ffab55998
Isis Huang <ihuang@nisum.com>
2022-08-29 17:41:11 +0000

    calculate power bill

---
+++ PowerBillCreation.java
@@ -0,0 +1,21 @@
+public class PowerBillCreation {
+    private int units;
+
+    PowerBillCreation(int units) {
+        this.units = units;
+    }
+
+    void calculateBill() {
+        double bill = 0;
+        if(units < 100) {
+            bill = units * 1.20;
+        }
+        else if(units < 300) {
+            bill = 100 * 1.20 + (units - 100) * 2;
+        }
+        else if(units > 300) {
+            bill = 100 * 1.20 + 200 * 2 + (units - 300) * 3;
+        }
+        System.out.println("The power bill for " + units + " is $" + bill);
+    }
+}
